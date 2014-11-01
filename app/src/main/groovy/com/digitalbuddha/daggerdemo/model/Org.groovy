268ef0bdd119cd33c5bdc8d9a0b2744831fd4427
MikeN <mike@touchlab.co>
2014-11-01 14:43:43 -0400

    RxJava and Groovy Support, dropped a lot of other libraries

---
+++ app/src/main/groovy/com/digitalbuddha/daggerdemo/model/Org.groovy
@@ -0,0 +1,11 @@
+package com.digitalbuddha.daggerdemo.model
+
+import groovy.transform.CompileStatic;
+
+/**
+ * Created by MikeN on 8/16/14.
+ */
+@CompileStatic
+public class Org {
+     String type
+}
